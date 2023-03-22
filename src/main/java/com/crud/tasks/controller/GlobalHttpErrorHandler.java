d0bad6ed62732fd0ecca92c065e2feb846d099ed
Jacek Rozdolski <jacekrozdolski@gmail.com>
2023-03-22 21:36:37 +0100

    15.2 Task: searching for a single task

---
+++ src/main/java/com/crud/tasks/controller/GlobalHttpErrorHandler.java
@@ -0,0 +1,15 @@
+package com.crud.tasks.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@ControllerAdvice
+class GlobalHttpErrorHandler extends ResponseEntityExceptionHandler {
+    @ExceptionHandler(TaskNotFoundException.class)
+    public ResponseEntity<Object> handleTaskException(TaskNotFoundException exception) {
+        return new ResponseEntity<>("Task with given id doesn't exist", HttpStatus.BAD_REQUEST);
+    }
+}
---
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -0,0 +1,30 @@
+package com.crud.tasks.service;
+
+import com.crud.tasks.controller.TaskNotFoundException;
+import com.crud.tasks.domain.Task;
+import com.crud.tasks.repository.TaskRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DbService {
+    @Autowired
+    private TaskRepository repository;
+
+    public List<Task> getAllTasks() {
+        return repository.findAll();
+    }
+
+    public Task saveTask(final Task task) {
+        return repository.save(task);
+    }
+
+    public Task getTask(final Long taskId) throws TaskNotFoundException {
+        return repository.findById(taskId).orElseThrow(TaskNotFoundException::new);
+    }
+
+}
